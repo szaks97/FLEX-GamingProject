8da8ce86ab6e6a913b7111d0f7f88ae891460c4b
sari zaks <sarihof1997@gmail.com>
2021-12-18 23:29:38 +0200

    Building Scene1 - stage_1

---
+++ Assets/Scripts/LaserBeams.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LaserBeams : MonoBehaviour
+{
+    private LineRenderer lr;
+   
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, new Vector3(hit.distance, 0, 0));
+            }
+
+            else
+            {
+                lr.SetPosition(1, new Vector3(50, 0, 0));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LaserSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LaserSettings : MonoBehaviour
+{
+    public GameObject sensorEdge;
+    
+    void Laser()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(sensorEdge.transform.position, sensorEdge.transform.forward, out hit))
+        {
+            if(hit.collider.tag == "Player")
+            {
+                print("Hit : " + hit.collider.gameObject.name);
+            }
+        }
+    }
+}
